e570ef92f32f18a541094386fadda237ba78d5e3
Astrid Massuh <Astridmassuh@users.noreply.github.com>
2020-08-23 22:57:37 -0400

    accommodations prototype

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AppContextProvider } from './context/AppContext';
 import ContextDemo from './components/ContextDemo';
+import Accommodations from './components/Accommodations';
 
 import './App.css';
 
@@ -16,13 +17,16 @@ const App = () => {
   useEffect(fetchDemoData, []);
 
   return (
-    <AppContextProvider>
-      <div id="demo">
-        <h3>Hello from client/src/App.js</h3>
-        <ContextDemo />
-        <h3>{serverMessage}</h3>
-      </div>
-    </AppContextProvider>
+    // <AppContextProvider>
+    //   <div id="demo">
+    //     <h3>Hello from client/src/App.js</h3>
+    //     <ContextDemo />
+    //     <h3>{serverMessage}</h3>
+    //   </div>
+    // </AppContextProvider>
+    <React.Fragment>
+      <Accommodations />
+    </React.Fragment>
   );
 };
 
---
+++ client/src/components/Accommodations.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Carousel, Button, Container, Row, Col } from 'react-bootstrap';
+
+function Accommodations() {
+  return (
+    <Container>
+      <Row className="accommodations-header">
+        <Carousel className="carousel-slide">
+          <Carousel.Item>
+            <img
+              className="d-block"
+              src="https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block"
+              src="https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block"
+              src="https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row>
+        <Col>
+          <Row>
+            <h1>Mandala Cabin</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div>97% - 145 Reviews </div>
+            <div className="stars">⭐️⭐️⭐️⭐️⭐️</div>
+          </Row>
+          <Row>Amenities</Row>
+          <Row>Reviews</Row>
+        </Col>
+        <Col>
+          <h3>$100</h3>
+          <p>per night</p>
+          <Button variant="link">Check in</Button>
+          <Button variant="link">Check out</Button>
+          <Button variant="link">Guests</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Accommodations;
