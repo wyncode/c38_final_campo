9f8cdaf474ae35e63928b7849ad9d7f03752a7b1
Astrid Massuh <Astridmassuh@users.noreply.github.com>
2020-08-30 21:46:47 -0400

    connected accommodations to api

--- client/src/components/Accommodations.jsx
+++ client/src/components/Accommodations.jsx
@@ -1,118 +1,132 @@
 import React, { useState } from 'react';
 import { Carousel, Button, Container, Row, Col, Image } from 'react-bootstrap';
 import 'react-dates/initialize';
-import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import { DateRangePicker} from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 import '../App.css';
+import axios from 'axios';
 
-function Accommodations() {
-  const [dates, setDates] = useState({startDate: null, endDate: null, focusedInput:null});
+class Accommodations extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate:null,
+      facilityID: 'test'
+    }
+  }
+  componentDidMount(){
+    axios.get(`/api/accommodations/${this.state.facilityID}`)
+    .then((data) =>{
+      this.setState({title: data.data.Title})
+      this.setState({overview: data.data.Overview})
+      this.setState({recreation: data.data.Recreation})
+      this.setState({facilities: data.data.Facilities})
+      this.setState({natural_features: data.data.Natural_Features})
+      this.setState({nearby_attractions: data.data.Nearby_Attractions})
+      this.setState({directions: data.data.Directions})
+      this.setState({attributes: data.data.Attributes})
+      this.setState({activites: data.data.Activities})
+      this.setState({images: data.data.Images})
+    })
+    .catch((error) => {console.log(error)})
+  }
+  render(){
+    let images;
+    let amenities;
+    if (this.state.images){
+      images = this.state.images.map(item =>(
+        <Carousel.Item>
+          <Image
+            src={item}
+          />
+        </Carousel.Item>));
+    }
+    if (this.state.attributes){
+      amenities = this.state.attributes.map(item =>(
+        <li>{item}</li>
+      ));
+    }
 
-  return (
-    <Container className="detail-container">
-      <Row className="accommodations-header">
-        <Carousel className="carousel-slide">
-          <Carousel.Item>
-            <Image
-              src={require("../assets/images/inside-tent.jpeg")}
-              alt="First slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src={require("../assets/images/inside-tent.jpeg")}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src={require("../assets/images/inside-tent.jpeg")}
-              alt="Third slide"
-            />
-          </Carousel.Item>
-        </Carousel>
-      </Row>
-      <Row>
-        <Col>
-          <Row className="detail-title">
-              <Col>
-                <Row><h1>Mandala Cabin</h1></Row>
-                <Row className="detail-review">
-                  <p>97%{' '}<span>(145)</span>Reviews ⭐️⭐️⭐️⭐️⭐️</p>
-                </Row>
-              </Col>
-          </Row>
-          <Row className="detail-title-bar"></Row>
-          <Row>
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </Row>
-          <Row className="detail-campsite">
-            <Col>
-              <Row>
-                <h3>Campsite Area</h3>
-                
+    return (
+      <Container className="detail-container">
+        <Row className="accommodations-header">
+          <Carousel className="carousel-slide">
+            {images}
+          </Carousel>
+        </Row>
+        <Row>
+          <Col xs={8}>
+            <Row className="detail-title">
                 <Col>
-               
-                  <ul>
-                  <li>ADA access</li>
-                  <li>Parking at listing</li>
-                  <li>Walk to listing</li>
-                  </ul>
+                  <Row><h1>{this.state.title}</h1></Row>
+                  <Row className="detail-review">
+                    <p>97%{' '}<span>(145)</span>Reviews ⭐️⭐️⭐️⭐️⭐️</p>
+                  </Row>
                 </Col>
-                </Row>
+            </Row>
+            <Row className="detail-title-bar"></Row>
+            <Row className="detail-description">
+              <p>{this.state.overview}</p>
+              <p>{this.state.facilities}</p>
+            </Row>
+            <Row className="detail-title-bar"></Row>
+            <Row className="detail-campsite">
+              <Col>
+                <div className="detail-campsite-card-outter">
+                  <div className="detail-campsite-card">
+                    <Row><h3>Amenities</h3></Row> 
+                    <Row>
+                      <ul>
+                        {amenities}
+                      </ul>
+                    </Row>
+                  </div>
+                </div>
                 
+              </Col>
+            </Row>
+            <Row className="detail-title-bar"></Row>
+            <Row className="detail-activities">
+              <Col>
+                <Row><h2>Activities</h2></Row>
+                <Row><h3>Offered near campground</h3></Row>
+                <div className="detail-campsite-card-outter">
+                  <div className="detail-campsite-card">
+                    <Row><h4>Biking</h4></Row>
+                    <Row>
+                      <p>Biking trails are available in this area. Bring your own mountain bike or rent one from our rental locations. </p>
+                    </Row>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col className="detail-booking">
+            <div className="detail-booking-card">
+              <Row className="detail-booking-card-price"><h3>$76/night</h3></Row>
               <Row>
-                <h3>Essentials</h3>
-                <ul>
-                  <li>Campfires allowed</li>
-                  <li>Toilet available</li>
-                  <li>No pets</li>
-                </ul>
+                <DateRangePicker 
+                  startDate={this.state.startDate}
+                  startDateId="your_unique_start_date_id"
+                  endDate={this.state.endDate}
+                  endDateId="your_unique_end_date_id"
+                  onDatesChange={({ startDate, endDate }) => this.setState({startDate, endDate})}
+                  focusedInput={this.state.focusedInput}
+                  onFocusChange={focusedInput => this.setState({focusedInput})}
+                />
               </Row>
               <Row>
-                <h3>Amenties</h3>
-                <ul>
-                  <li>Picnic table available</li>
-                  <li>Portable water available</li>
-                  <li>No hot showers</li>
-                </ul>
+                <Button className="book-button-monkey"> Book Now </Button>
               </Row>
-            </Col>
-          </Row>
-        </Col>
-
-        <Col className="detail-booking">
-          <Row>
-            <h3>$76/night</h3>  
-          </Row>
-          <Row>
-            
-            <DateRangePicker 
-              startDate={dates.startDate}
-              startDateId="your_unique_start_date_id"
-              endDate={dates.endDate}
-              endDateId="your_unique_end_date_id"
-              onDatesChange={({ startDate, endDate }) => setDates({startDate, endDate})}
-              focusedInput={dates.focusedInput}
-              onFocusChange={focusedInput => setDates({focusedInput})}
-            />
-
-          </Row>
-          <Row>
-            <Button className="book-button-monkey"> Book Now </Button>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-  );
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
+
 
 export default Accommodations;
--- server/routes/open/index.js
+++ server/routes/open/index.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const router = require('express').Router(),
   { sendWelcomeEmail, forgotPasswordEmail } = require('../../emails/'),
   jwt = require('jsonwebtoken'),
@@ -88,5 +89,72 @@ router.get('/api/password/:token', (req, res) => {
     res.status(401).json({ error: error.toString() });
   }
 });
+
+// ******************************
+// Collect data for accommodations
+// ******************************
+router.get('/api/accommodations/:facilityId', (req,res) => {
+  let {facilityId} = req.params;
+  let campsite = {};
+  if (facilityId === "test"){
+    facilityId = "246895";
+  }
+  try {
+    axios.get(`https://ridb.recreation.gov/api/v1/facilities/${facilityId}?apikey=df27448b-db46-4814-b2d8-d54214186c6e`)
+    .then((facility_data) =>{
+      campsite['Title'] = facility_data.data.FacilityName;
+      let description = facility_data.data.FacilityDescription.replace('<br/><br/>', '')
+      description = description.split('<h2>Overview</h2>\n')[1]
+      description = description.split('<h2>Recreation</h2>\n');
+      campsite['Overview'] = description[0];
+      description = description[1].replace('<br/><br/>', '');
+      description = description.split('<h2>Facilities</h2>\n');
+      campsite['Recreation'] = description[0]
+      description = description[1].replace('<br/><br/>', '');
+      description = description.split('<h2>Natural Features</h2>\n');
+      campsite['Facilities'] = description[0]
+      description = description[1].replace('<b>', '');
+      description = description.replace('</b>\n', '');
+      description = description.split('<h2>Nearby Attractions</h2>\n');
+      campsite['Natural_Features'] = description[0]
+      campsite['Nearby_Attractions'] = description[1]
+      campsite['Directions'] = facility_data.data.FacilityDirections
+      const recId = facility_data.data.ParentRecAreaID
+      axios.get(`https://ridb.recreation.gov/api/v1/facilities/${facilityId}/campsites?apikey=df27448b-db46-4814-b2d8-d54214186c6e`)
+      .then((campsites_data) =>{
+        campsite['Attributes'] = campsites_data.data['RECDATA'][0]['ATTRIBUTES'];
+        axios.get(`https://ridb.recreation.gov/api/v1/recareas/${recId}/activities?apikey=df27448b-db46-4814-b2d8-d54214186c6e`)
+        .then((activity_data) =>{
+          campsite['Activities'] = activity_data.data['RECDATA'];
+          axios.get(`https://ridb.recreation.gov/api/v1/facilities/${facilityId}/media?apikey=df27448b-db46-4814-b2d8-d54214186c6e`)
+          .then((medias_data) => {
+            let images = [];
+            medias_data.data['RECDATA'].map((item)=>{
+              images.push(item.URL);
+            })
+            campsite['Images'] = images;
+            res.send(campsite);
+          })
+          .catch((error)=>{
+            console.log(error);
+          })
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+  catch(error){
+    console.log(error);
+  }
+});
+
 
 module.exports = router;
