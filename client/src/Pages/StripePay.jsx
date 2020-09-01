0fc279f7acbe00dc8561a53179d3aead53965a07
Priscilla <misspstandard@yahoo.com>
2020-09-01 00:56:34 -0400

    fixed route to checkout page, added styling, added image to public images folder

--- client/src/Pages/StripePay.jsx
+++ client/src/Pages/StripePay.jsx
@@ -1,25 +1,67 @@
 import React from 'react';
-
-
-import StripeCheckoutButton from '../components/stripe.checkout';
+import NavigationBar from '../components/NavigationBar';
+import StripeCheckoutButton from '../components/StripeCheckout';
+import './Pay.css';
 
 const Stripe = () => {
-
+  const pricePerNight = 90;
+  const numberOfNights = 3;
   const totalPrice = 58;
 
   return (
-    <div className="stripe">
-      <header className="App-header">
-        <h1>Make Stripe Payment to Campo</h1>
-        <p>
-          Pay Total of $ {totalPrice}
-        </p>
-        <p>
+    <div id="paypage">
+      <NavigationBar />
+      <div className="stripe">
+        <header className="App-header">
+          <h2>Review Your Trip</h2>
+          <div id="parkDetails">
+            <img
+              src="/images/Yellowstone.jpg"
+              height="134px"
+              width="150px"
+            ></img>
+            <div id="parknamespace">
+              <h5>Yellowstone National Park</h5>
+
+              <h6>${pricePerNight}/night</h6>
+            </div>
+          </div>
+          <div></div>
+          <div className="borders right">
+            <h4>Check in</h4>
+            <p>Jan 1, 2021</p>
+          </div>
+          <div className="borders">
+            <h4>Check out</h4>
+            <p>Jan 4, 2021</p>
+          </div>
+          <h4>Contact Info</h4>
+          <p>Stefani Kahle</p>
+          <p>stefanikahle@gmail.com</p>
+          <p>555-435-3295</p>
+
+          <h4>Payment details</h4>
+          <h6>
+            ${pricePerNight} x {numberOfNights} nights
+          </h6>
+
+          <div id="fees">
+            <span>Park Fees</span>
+            <span>$200.00</span>
+          </div>
+          <div id="feesborder">
+            <span>Taxes</span>
+            <span>$40.00</span>
+          </div>
+          <div id="fees">
+            <span>Total</span>
+            <span>$240.00</span>
+          </div>
           <StripeCheckoutButton price={totalPrice} />
-        </p>
-      </header>
+        </header>
+      </div>
     </div>
   );
-}
+};
 
 export default Stripe;
