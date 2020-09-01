7aa56d65aa57b28190b732a386eaa0d17c4b0d66
garandrew102 <garandrew102@gmail.com>
2020-08-31 21:18:37 -0400

    signin/signup page style

--- client/src/Pages/SigninPage.jsx
+++ client/src/Pages/SigninPage.jsx
@@ -11,9 +11,9 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
+    <div id="body">
       <NavigationBar />
-      <Container className="container d-flex flex-column align-items-center justify-content-center">
+      <Container id="signin">
         <h1>Welcome to Campo</h1>
         <Form style={{ width: 300 }}>
           <Form.Group controlId="formBasicEmail">
@@ -34,13 +34,15 @@ const LoginPage = () => {
             />
           </Form.Group>
           <Form.Group className="d-flex justify-content-center">
-            <Button type="submit">Sign In</Button>
+            <Button id="buttons" type="submit">
+              Sign In
+            </Button>
           </Form.Group>
         </Form>
-        <Link className="mt-2" to="/reset-password">
+        <Link id="fontstyle" className="mt-2" to="/reset-password">
           Forgot Password?
         </Link>
-        <Link className="mt-2" to="/signup">
+        <Link id="fontstyle" className="mt-2" to="/signup">
           Create an Account
         </Link>
       </Container>
