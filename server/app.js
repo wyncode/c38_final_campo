86a9625eeade0e3c8605be4a06096dacc2d5b9e4
garandrew102 <garandrew102@gmail.com>
2020-09-08 15:54:57 -0400

    commit

--- server/app.js
+++ server/app.js
@@ -102,10 +102,10 @@ app.use('/api/bookings', bookingRouter);
 
 app.use(globalErrorHandler);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.get('*', (request, response) => {
-//     response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 module.exports = app;
